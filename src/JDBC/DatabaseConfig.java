61b8d09047cda65e2ce3b9769458ccef01c0424a
BOUTCHOUANG1 <boutchouangelija@gmail.com>
2025-01-17 13:40:14 +0100

    updated library management system

--- src/JDBC/DatabaseConfig.java
+++ src/JDBC/DatabaseConfig.java
@@ -8,7 +8,7 @@ public class DatabaseConfig {
     private static final Properties properties = new Properties();
 
     static {
-        try (InputStream input = DatabaseConfig.class.getClassLoader().getResourceAsStream("JDBC/db.properties")) {
+        try (InputStream input = DatabaseConfig.class.getClassLoader().getResourceAsStream("resources/db.properties")) {
             if (input == null) {
                 System.out.println("Sorry, unable to find db.properties");
                 System.exit(1);
